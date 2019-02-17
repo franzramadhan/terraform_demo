1a766781d25e18d50d7bf78e500a1e14fe209d36
franzramadhan-ts <franz@trustingsocial.com>
2019-02-17 20:40:39 +0700

    added travis

--- terraform/gcp/variables.tf
+++ terraform/gcp/variables.tf
@@ -1,12 +1,16 @@
 variable "project_id" {}
-variable "region" {}
+variable "region" {
+    default = "asia-southeast-1"
+}
+variable "zone" {
+    default = "asia-southeast1-a"
+}
 variable "project_name" {
     default = "terraform-gcp"
 }
 variable "machine_type" {
     default = "f1-micro"
 }
-variable "zone" {}
 variable "image_family" {
     default = "centos-7"
 }
